deea46ad6e229261dc6155c456ffaea85256b8b2
Young <hogusong1@gmail.com>
2018-09-07 23:57:25 -0700

    finish Home page of ionicReddit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { RedditsPage } from '../pages/reddits/reddits';
 import { SettingsPage } from '../pages/settings/settings';
+import { DetailsPage } from '../pages/details/details';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -17,10 +19,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     RedditsPage,
     SettingsPage,
+    DetailsPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -29,6 +33,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     RedditsPage,
     SettingsPage,
+    DetailsPage,
     TabsPage
   ],
   providers: [
--- src/pages/reddits/reddits.ts
+++ src/pages/reddits/reddits.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RedditService } from '../../app/services/reddit.service';
+import { DetailsPage } from '../../pages/details/details';
 
 @Component({
-  templateUrl: 'reddits.html'
+  selector: 'reddits',
+  templateUrl: 'reddits.html',
 })
 export class RedditsPage {
+  items: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private redditService: RedditService) {
 
   }
 
+  ngOnInit() {
+    this.getPosts('sports', 20);
+  }
+
+  getPosts(catergory, limit) {
+    this.redditService.getPosts(catergory, limit).subscribe(response => {
+      this.items = response.data.children;
+    })
+  }
+
+  viewItem(item) {
+    // console.log(item.preview.images[0].source)
+    let newURL: string = item.preview.images[0].source.url.replace(/&/g,"&");
+    this.navCtrl.push(DetailsPage, {
+      item: item,
+      url: newURL
+    })
+  }
 }
